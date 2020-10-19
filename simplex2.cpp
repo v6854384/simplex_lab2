e927fe644e59d606f52bcc0bf180bbb4957816d0
Viktor <32385043+v6854384@users.noreply.github.com>
2020-10-19 17:37:28 +0300

    Update simplex2.cpp

--- simplex2.cpp
+++ simplex2.cpp
@@ -9,9 +9,9 @@
  
 typedef std::vector<std::vector<double>> Arr;
  
-void print(const Arr& table);
+void print(const Arr& table, bool isSpec = false);
 bool isContainNeg(const std::vector<double>& table);
-void solve(Arr table);
+void solve(Arr& table, bool isSpec = false);
  
 int main()
 {
@@ -22,13 +22,14 @@ int main()
         {
             {0,  0,   1,  2,   3},
             {4,  4,   4,  1,   1},
-            {5,  3,   1,  2,   0},
+            {5,  3,   3,  1,   0},
             {6,  2,   0, 0.5,  1},
-            {0,  0,   7,  5,   3}
+            {0,  0,   7,  5,   3}   //коэффициенты взяты по модулю 
         }
     };
  
     solve(table);
+    std::cout << "Значение целевой функции(максимизация):" << -table[table.size() - 1][1] << '\n';
  
     //составим двойственную задачу    
  
@@ -36,27 +37,32 @@ int main()
     {
         {
             {0,  0,   1,   2,   3},
-            {4,  -7,   -4,   -1,   0},
-            {6,  -5,   -1,   -2,   -0.5},
-            {7,  -3,   -1,   0,   -1},
-            {0,  0,   -4,   -3,   -2}
+            {4,  -7,   -4,   -3,   0},
+            {5,  -5,   -1,   -1,   -0.5},
+            {6,  -3,   -1,   0,   -1},
+            {0,  0,   4,   3,   2}
         }
     };
  
     std::cout << "Dvoystvenna9 zada4a:\n";
  
-    solve(table);
+    solve(table, true);
     std::cin.get();
     std::cin.get();
     return 0;
 }
  
-void solve(Arr table)
+void solve(Arr& table, bool isSpec)
 {
- 
     const std::size_t N = table.size();
     const std::size_t M = table[0].size();
-    print(table);
+    print(table, isSpec);
+ 
+    std::vector<double> C((M - 2) * 2);
+    for (std::size_t i = 0; i < M - 2; i++)
+    {
+        C[i] = table[N - 1][2 + i];
+    }
  
     while (true)
     {
@@ -69,6 +75,7 @@ void solve(Arr table)
             {
                 min = table[j][1];
                 Vrow = j;
+                break;
             }
         }
  
@@ -82,6 +89,7 @@ void solve(Arr table)
                 {
                     min = table[Vrow][j];
                     Vcol = j;
+ 
                 }
             }
             if (min == 0)
@@ -93,26 +101,33 @@ void solve(Arr table)
         }
         else
         {
+            double max = 0;
             for (std::size_t j = 2; j < M; j++)
             {
-                if (table[N - 1][j] > 0)
+                if (table[N - 1][j] > max)
                 {
                     Vcol = j;
-                    break;
+                    max = table[N - 1][j];
                 }
             }
             if (Vcol == 0)
             {
                 std::cout << "Получено оптимальное решение\n";
+                double F = 0;
+                for (std::size_t i = 1; i < N - 1; i++)
+                {
+                    F += table[i][1] * C[static_cast<std::size_t>(table[i][0]) - 1];
+                }
+                std::cout << "Значение целевой функции " << F << '\n';
                 return;
             }
-            std::cout << "Ведущим выбран столбец X" << table[0][Vcol] << "(первый положительный)\n";
+            std::cout << "Ведущим выбран столбец X" << table[0][Vcol] << "(максимальный положительный)\n";
             min = std::numeric_limits<double>::max();
             for (std::size_t j = 1; j < N - 1; j++)
             {
                 //минимальное положительное отношение свободного члена
                 auto d = table[j][1] / table[j][Vcol];
-                if (d > 0 && min > d)
+                if (table[j][Vcol] > 0 && d >= 0 && min > d)
                 {
                     min = d;
                     Vrow = j;
@@ -156,14 +171,33 @@ void solve(Arr table)
  
         std::swap(table[0][Vcol], table[Vrow][0]);
  
-        print(table);
+ 
+        //работает только для конкретной симплекс-таблицы
+        if (table[table.size() - 1][1] == -20)
+        {
+            for (std::size_t j = 2; j < table[0].size(); j++)
+            {
+                table[table.size() - 1][j] *= -1;
+            }
+            table[table.size() - 1][1] = 0;
+            print(table, isSpec);
+        }
+        else
+        {
+            print(table, isSpec);
+        }
+        double F = 0;
+        for (std::size_t i = 1; i < N - 1; i++)
+        {
+            F += table[i][1] * C[static_cast<std::size_t>(table[i][0]) - 1];
+        }
+        std::cout << "Значение целевой функции " << F << '\n';
     }
 }
  
 //вывод симплекс-таблицы
-void print(const Arr& table)
+void print(const Arr& table, bool isSpec)
 {
- 
     std::cout << std::right << std::setw(8) << "" << "  ";
     std::cout << std::right << std::setw(8) << "Si0" << "  ";
     for (std::size_t j = 2; j < table[0].size(); j++)
@@ -188,8 +222,10 @@ void print(const Arr& table)
         }
         std::cout << '\n';
     }
- 
-    std::cout << "Значение целевой функции " << table[table.size() - 1][1] << '\n';
+    if (!isSpec)
+    {
+        std::cout << "Значение целевой функции " << table[table.size() - 1][1] << '\n';
+    }
 }
  
 //если есть хотя-бы 1 отрицательный элемент -
